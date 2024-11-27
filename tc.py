716cb4cdc8c6d6f6e6c8d0f1d86859a9c88588fd
musfi64 <Asher786@yahoo.com>
2024-11-27 17:12:31 -0600

    paythone homework

---
+++ tc.py
@@ -0,0 +1,19 @@
+a=int(input("enter a your name"))
+b=float(a)
+print(b)
+
+c=float(input("enter a your bith date"))
+d=int(c)
+print(d)
+
+e=int(input("enter a your hight"))
+f=float(e)
+print(f)
+
+g=float(input("enter a your hobby"))
+h=int(g)
+print(h)
+
+i=int(input("enter a your dream"))
+j=float(i)
+print(j)
